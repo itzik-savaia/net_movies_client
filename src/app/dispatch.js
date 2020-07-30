2b84bea58d0c879099dc45972056e382d3f36ea6
itzik savaia <44684932+itzikking@users.noreply.github.com>
2020-07-30 06:15:35 +0300

    as

--- src/app/dispatch.js
+++ src/app/dispatch.js
@@ -6,10 +6,7 @@ export const Fetch_All_Movies = () => {
     return (dispatch) => {
         axios.get("https://localhost:44362/api/movie")
             .then((response) => {
-                const movies = Object.keys(response.data).map(key =>
-                    response.data[key]
-                )
-                dispatch({ type: 'FETCH_ALL_MOVIES', payload: movies })
+                dispatch({ type: 'FETCH_ALL_MOVIES', payload: response.data })
             }).catch((err) => {
                 dispatch({ type: 'FETCH_ALL_ERROR', payload: err })
             })
