f809670fc3d4ca67a6a14f3b5a274356cef0535d
itzik savaia <44684932+itzikking@users.noreply.github.com>
2020-07-30 06:14:33 +0300

    as

--- src/layouts/home/view.js
+++ src/layouts/home/view.js
@@ -41,38 +41,6 @@ const View = props => {
                 <Grid item>
                 </Grid>
             </Grid>
-            {/* <h1 style={{ color: 'red' }}>movies</h1> */}
-            {/* {movieList.map((movie, i) => (
-                        <div key={movie.props.children.id} className="carousel-cell">
-                            <div className="movie_card" key={movie.props.children.id} id={movie.props.children.name}>
-                                <a onClick={click_movie(movie.props.children.name)}>
-                                    <div className="info_section">
-                                        <div className="movie_header">
-                                            {movie.props.children.photos.map(photo => (
-                                                <img key={movie.props.children.id} className="locandina" src={photo.small_Picture} />
-                                            ))}
-                                            <h1>{movie.props.children.name}</h1>
-                                            <h4>{movie.props.children.publishing_Year}, {movie.props.children.stage_Manager}</h4>
-                                            <span className="minutes">{movie.props.children.minutes} min</span>
-                                            <p className="type">{movie.props.children.types}</p>
-                                        </div>
-                                        <div className="movie_desc">
-                                            <p className="text">{movie.props.children.text}</p>
-                                        </div>
-                                        <div className="movie_social">
-                                            <ul>
-                                                <li><i className="material-icons">like</i></li>
-                                                <li><i className="material-icons">photos</i></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                    {movie.props.children.photos.map(photo => (
-                                        <div key={movie.props.children.id} className="blur_back" style={{ backgroundImage: `url(${photo.background})` }}></div>
-                                    ))}
-                                </a>
-                            </div>
-                        </div>
-                    ))} */}
         </div >
     );
 }
